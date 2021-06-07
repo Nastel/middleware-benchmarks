daea17b57564caec901cdcbd5356a649221124b7
danielrigos <drigos@nastel.com>
2021-06-07 11:06:19 -0400

    Initial Consumer class that works, could be improved

--- src/main/java/com/kafka/Consumer.java
+++ src/main/java/com/kafka/Consumer.java
@@ -1,11 +1,38 @@
 package com.kafka;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
 public class Consumer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// test new branch
+		Properties properties = new Properties();
+		properties.put("bootstrap.servers", "localhost:9092"); //Change IP depending on producer
+		properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		properties.put("group.id", "test-group");
 
+		KafkaConsumer kafkaConsumer = new KafkaConsumer(properties);
+		List topics = new ArrayList();
+		topics.add("newKafkaTest"); //Change Topic depending on producer
+		kafkaConsumer.subscribe(topics);
+		try {
+			while (true) {
+				ConsumerRecords records = kafkaConsumer.poll(10);
+				for (Object record : records) {
+					System.out.println(
+							String.format("Topic - %s, Partition - %d, Value: %s", ((ConsumerRecord) record).topic(),
+									((ConsumerRecord) record).partition(), ((ConsumerRecord) record).value()));
+				}
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			kafkaConsumer.close();
+		}
 	}
-
 }
