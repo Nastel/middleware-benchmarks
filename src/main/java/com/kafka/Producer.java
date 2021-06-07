6a8886d404931d31288cd9ba710485f8d4f5f61f
Spencer Spitz <spencerspitz@gmail.com>
2021-06-07 10:21:41 -0400

    basic kafka producer

--- src/main/java/com/kafka/Producer.java
+++ src/main/java/com/kafka/Producer.java
@@ -1,10 +1,38 @@
 package com.kafka;
 
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
 public class Producer {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public static void main(String args[]) {
+		Producer.basicSend();
 	}
 
+	private static Properties producerProps(String bootstrapServer) {
+		String serializer = StringSerializer.class.getName();
+		Properties props = new Properties();
+		props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+		props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, serializer);
+		props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, serializer);
+
+		return props;
+	}
+
+	public static void basicSend() {
+		String bootstrapServer = "localhost:9092";
+		String topic = "MyKafkaBenchmark";
+		KafkaProducer<String, String> kafkaProducer = new KafkaProducer<String, String>(producerProps(bootstrapServer));
+		try {
+			kafkaProducer.send(new ProducerRecord<String, String>(topic, "key", "value"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			kafkaProducer.close();
+		}
+	}
 }
