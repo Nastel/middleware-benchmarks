5f2a597a32d431c119aa32d78eba6454e20f792c
danielrigos <drigos@nastel.com>
2021-06-08 09:46:43 -0400

    Updated Consumer.java, still has while(true) loop

--- src/main/java/com/kafka/Consumer.java
+++ src/main/java/com/kafka/Consumer.java
@@ -1,8 +1,16 @@
-package com.kafka;
+package kafkatester;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -13,29 +21,37 @@ public class Consumer {
 		Consumer.receive();
 	}
 	public static void receive() {
+		
 		Properties properties = new Properties();
-		properties.put("bootstrap.servers", "localhost:9092"); // Change IP depending on producer
+		properties.put("bootstrap.servers", "localhost:9092"); // Change IP depending on producer 11.0.0.173
 		properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 		properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 		properties.put("group.id", "test-group");
-		
+		properties.put("session.timeout.ms", "10000");
+		properties.put("auto.offset.reset", "earliest");
+
 		KafkaConsumer kafkaConsumer = new KafkaConsumer(properties);
 		List topics = new ArrayList();
-		topics.add("newKafkaTest"); // Change Topic depending on producer
+		topics.add("newKafkaTest"); // Change Topic depending on producer consumeTrial1
 		kafkaConsumer.subscribe(topics);
-		try {
-			while (true) {
+		//CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+		//ExecutorService service = Executors.newFixedThreadPool(5);
+		//Future<Integer> future = service.submit(new Task());
+			try {
+				while(true) {
 				ConsumerRecords records = kafkaConsumer.poll(10);
 				for (Object record : records) {
 					System.out.println(
 							String.format("Topic - %s, Partition - %d, Value: %s", ((ConsumerRecord) record).topic(),
 									((ConsumerRecord) record).partition(), ((ConsumerRecord) record).value()));
+					//kafkaConsumer.close(20, TimeUnit.MICROSECONDS);
 				}
+				}} catch (Exception e) {
+				System.out.println(e.getMessage());
+			} finally {
+				kafkaConsumer.close();
 			}
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		} finally {
-			kafkaConsumer.close();
-		}
-	}
-}
+		}}//);
+	
+
+
