a71d245059490b04dca86b2d71ec97d9ef829f1d
Spencer Spitz <spencerspitz@gmail.com>
2021-06-08 10:42:21 -0400

    updated package

--- src/main/java/com/kafka/Consumer.java
+++ src/main/java/com/kafka/Consumer.java
@@ -1,4 +1,4 @@
-package kafkatester;
+package com.kafka;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,30 +21,31 @@ public class Consumer {
 	public static void main(String[] args) {
 		Consumer.receive();
 	}
+
 	public static void receive() {
-		
+
 		Properties properties = new Properties();
 		properties.put("bootstrap.servers", "localhost:9092"); // Change IP depending on producer 11.0.0.173
 		properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 		properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 		properties.put("group.id", "new-group4S");
-		//properties.put("session.timeout.ms", "30000");
-		//properties.put("auto.offset.reset", "earliest");
-		//properties.put("enable.auto.commit", "false");
-		//properties.put("auto.commit.interval.ms", "1000");
+		// properties.put("session.timeout.ms", "30000");
+		// properties.put("auto.offset.reset", "earliest");
+		// properties.put("enable.auto.commit", "false");
+		// properties.put("auto.commit.interval.ms", "1000");
 		properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
 		properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 		properties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 10000);
-		
+
 		KafkaConsumer kafkaConsumer = new KafkaConsumer(properties);
 		List topics = new ArrayList();
 		topics.add("newKafkaTest"); // Change Topic depending on producer consumeTrial1
 		kafkaConsumer.subscribe(topics);
-		//CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
-		//ExecutorService service = Executors.newFixedThreadPool(5);
-		//Future<Integer> future = service.submit(new Task());
-			try {
-				while(true) {
+		// CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+		// ExecutorService service = Executors.newFixedThreadPool(5);
+		// Future<Integer> future = service.submit(new Task());
+		try {
+			while (true) {
 				ConsumerRecords records = kafkaConsumer.poll(10);
 				for (Object record : records) {
 					System.out.println(
@@ -53,14 +54,13 @@ public class Consumer {
 					if (((ConsumerRecord) record).value() == "end") {
 						kafkaConsumer.close();
 					}
-					//kafkaConsumer.close(20, TimeUnit.MICROSECONDS);
+					// kafkaConsumer.close(20, TimeUnit.MICROSECONDS);
 				}
-				}} catch (Exception e) {
-				System.out.println(e.getMessage());
-			} finally {
-				kafkaConsumer.close();
 			}
-		}}//);
-	
-
-
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			kafkaConsumer.close();
+		}
+	}
+}// );
