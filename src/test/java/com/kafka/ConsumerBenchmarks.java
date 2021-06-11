bd4be6f39aed6107f910d869d54c6f2e41e3a5f8
Spencer Spitz <spencerspitz@gmail.com>
2021-06-11 11:04:07 -0400

    fixed commented param

--- src/test/java/com/kafka/ConsumerBenchmarks.java
+++ src/test/java/com/kafka/ConsumerBenchmarks.java
@@ -31,7 +31,7 @@ public class ConsumerBenchmarks {
 	private ArrayList<String> topics;
 
 	// These are the topic names (named by the byte size of the messages in them)
-	@Param({ "512Size" }) // , "1024Size", "10240Size", "32768Size", "65536Size", "1024000Size"
+	@Param({ "512Size", "1024Size", "10240Size", "32768Size", "65536Size", "1024000Size" })
 	private String messageSize;
 
 	@Param({ "10000", "100000", "1000000" })
