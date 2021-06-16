ae233f065e7dde38ed1850124b26be5f826cfa8f
danielrigos <drigos@nastel.com>
2021-06-16 16:48:31 -0400

    Actual artemis code

--- src/main/java/com/activemq/artemis/ArtemisProducer.java
+++ src/main/java/com/activemq/artemis/ArtemisProducer.java
@@ -1,35 +1,64 @@
 package com.activemq.artemis;
 
 import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
 import javax.jms.DeliveryMode;
 import javax.jms.Destination;
 import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
+import javax.jms.Queue;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
-import org.apache.activemq.ActiveMQConnection;
-import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.artemis.api.core.TransportConfiguration;
+import org.apache.activemq.artemis.api.jms.ActiveMQJMSClient;
+import org.apache.activemq.artemis.api.jms.JMSFactoryType;
+import org.apache.activemq.artemis.core.remoting.impl.netty.NettyConnectorFactory;
 
 public class ArtemisProducer {
 
 	public static void main(String[] args) throws JMSException {
-		// TODO Auto-generated method stub
-	    String url = ActiveMQConnection.DEFAULT_BROKER_URL;
-        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
-        Connection connection = connectionFactory.createConnection();
-        connection.start();
-        
-        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        Destination destination = session.createQueue("Test.Queue");
-        MessageProducer producer = session.createProducer(destination);
-        producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-        String text = "Hey World";
-        TextMessage message = session.createTextMessage(text);
-        System.out.println("Trying to send message.....");
-        producer.send(message);
-        session.close();
-        connection.close();
+		TransportConfiguration transportConfiguration = new TransportConfiguration(NettyConnectorFactory.class.getName());
+
+		ConnectionFactory cf = ActiveMQJMSClient.createConnectionFactoryWithoutHA(JMSFactoryType.CF,transportConfiguration);
+
+		//We also create the JMS Queue object via the ActiveMQJMSClient Utility
+		//class:
+
+		Queue orderQueue = ActiveMQJMSClient.createQueue("OrderQueue");
+
+		//Next we create a JMS connection using the connection factory:
+
+		Connection connection = cf.createConnection();
+
+		//And we create a non transacted JMS Session, with AUTO\_ACKNOWLEDGE
+		//acknowledge mode:
+
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+		//We create a MessageProducer that will send orders to the queue:
+
+		MessageProducer producer = session.createProducer(orderQueue);
+
+		//And we create a MessageConsumer which will consume orders from the
+		//queue:
+
+		MessageConsumer consumer = session.createConsumer(orderQueue);
+
+		//We make sure we start the connection, or delivery won't occur on it:
+
+		connection.start();
+
+		//We create a simple TextMessage and send it:
+
+		TextMessage message = session.createTextMessage("This is an order");
+		producer.send(message);
+
+		//And we consume the message:
+
+		//TextMessage receivedMessage = (TextMessage)consumer.receive();
+		//System.out.println("Got order: " + receivedMessage.getText());
 	}
 
 }
