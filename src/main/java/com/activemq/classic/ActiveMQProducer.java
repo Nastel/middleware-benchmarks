eb5925356f05321ecc47e972984bbf6deccfda81
danielrigos <drigos@nastel.com>
2021-06-17 12:14:36 -0400

    Updated ActiveMQ Classic Consumer and Producer

--- src/main/java/com/activemq/classic/ActiveMQProducer.java
+++ src/main/java/com/activemq/classic/ActiveMQProducer.java
@@ -1,35 +1,67 @@
 package com.activemq.classic;
 
+import javax.jms.BytesMessage;
 import javax.jms.Connection;
 import javax.jms.DeliveryMode;
 import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
-import javax.jms.TextMessage;
-
 import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 public class ActiveMQProducer {
+	private final String QUEUE_NAME = "Test.Classic.Benchmark";
+	private Connection connection;
+	private Session session;
+	private MessageProducer producer;
+	private Destination destination;
 
+	
+	public void makeConnection() {
+		try {
+		    String url = ActiveMQConnection.DEFAULT_BROKER_URL;
+	        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
+	        connection = connectionFactory.createConnection();
+	        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+	        destination = session.createQueue(QUEUE_NAME);
+	        producer = session.createProducer(destination);
+	        producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+	        connection.start();
+
+		}catch(JMSException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public ActiveMQProducer() {
+		makeConnection();
+		
+	}
+	public void produce(int totalMessages, int msgSize) throws JMSException {
+		byte[] byteArrMsg = new byte[msgSize];
+		try {
+			BytesMessage message = session.createBytesMessage();
+			message.writeBytes(byteArrMsg);
+
+			for (int counter = 0; counter < totalMessages; counter++) {
+				producer.send(message);
+				System.out.println("Message " + counter + " sent to queue");
+			}
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void closeConnection() {
+		try {
+			connection.close();
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
 	public static void main(String[] args) throws JMSException {
-		// TODO Auto-generated method stub
-	    String url = ActiveMQConnection.DEFAULT_BROKER_URL;
-        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
-        Connection connection = connectionFactory.createConnection();
-        connection.start();
-        
-        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        Destination destination = session.createQueue("Test.Queue");
-        MessageProducer producer = session.createProducer(destination);
-        producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-        String text = "Hey World";
-        TextMessage message = session.createTextMessage(text);
-        System.out.println("Trying to send message.....");
-        producer.send(message);
-        session.close();
-        connection.close();
+
 	}
 
 }
