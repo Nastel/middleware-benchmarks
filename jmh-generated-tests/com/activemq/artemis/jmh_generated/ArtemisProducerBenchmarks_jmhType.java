8f614ced642722e0e07b0ae72352ee3213efd97b
Spencer Spitz <spencerspitz@gmail.com>
2021-06-17 10:56:53 -0400

    producer class and benchmark

---
+++ jmh-generated-tests/com/activemq/artemis/jmh_generated/ArtemisProducerBenchmarks_jmhType.java
@@ -0,0 +1,4 @@
+package com.activemq.artemis.jmh_generated;
+public class ArtemisProducerBenchmarks_jmhType extends ArtemisProducerBenchmarks_jmhType_B3 {
+}
+
--- src/test/java/com/activemq/artemis/ArtemisProducerBenchmarks.java
+++ src/test/java/com/activemq/artemis/ArtemisProducerBenchmarks.java
@@ -1,34 +1,76 @@
 
 package com.activemq.artemis;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+
+import com.gocypher.cybench.core.annotation.BenchmarkMetaData;
 
 @State(Scope.Benchmark)
+@BenchmarkMetaData(key = "isLibraryBenchmark", value = "true")
+@BenchmarkMetaData(key = "context", value = "SendMsg")
+@BenchmarkMetaData(key = "domain", value = "java")
+@BenchmarkMetaData(key = "version", value = "1.0.0")
+@BenchmarkMetaData(key = "description", value = "Middleware helps to deliver and track messages between systems")
 public class ArtemisProducerBenchmarks {
+	private ArtemisProducer myProducer;
 
+	@Param({ "10", "1000" })
+	private int totalProducedMessages;
 
-    @Setup(Level.Trial)
-    public void setup() {
-        //TODO Trial level: write code to be executed before each run of the benchmark
-    }
+	@Param({ "512", "1024", "10240", "32768", "65536" })
+	private int messageByteSize;
 
-    @Setup(Level.Iteration)
-    public void setupIteration() {
-        //TODO Iteration level: write code to be executed before each iteration of the benchmark.
-    }
+	@Setup(Level.Trial)
+	public void setup() {
+		myProducer = new ArtemisProducer();
+	}
 
-    @TearDown(Level.Trial)
-    public void tearDown() {
-        //TODO Trial level: write code to be executed after each run of the benchmark
-    }
+	@Setup(Level.Iteration)
+	public void setupIteration() {
+	}
 
-    @TearDown(Level.Iteration)
-    public void tearDownIteration() {
-        //TODO Iteration level: write code to be executed after each iteration of the benchmark.
-    }
+	@Benchmark
+	@BenchmarkMetaData(key = "api", value = "ActiveMQ Artemis")
+	@BenchmarkMetaData(key = "libVendor", value = "Apache")
+	@BenchmarkMetaData(key = "libUrl", value = "https://activemq.apache.org/components/artemis/")
+	@BenchmarkMetaData(key = "libVersion", value = "2.17.0")
+	@BenchmarkMetaData(key = "libDescription", value = "High-performance, non-blocking architecture for the next generation of messaging applications.")
+	@BenchmarkMetaData(key = "actionName", value = "produce")
+	@BenchmarkMetaData(key = "libSymbolicName", value = "org.apache.activemq.artemis-jms-client-all")
+	@BenchmarkMetaData(key = "title", value = "Sending messages")
+	@BenchmarkMode(Mode.Throughput)
+	@OutputTimeUnit(TimeUnit.SECONDS)
+	@Fork(1)
+	@Threads(1)
+	@Measurement(iterations = 100, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+	@Warmup(iterations = 5, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+	public void produce(Blackhole bh) {
+		myProducer.produce(totalProducedMessages, messageByteSize);
+	}
+
+	@TearDown(Level.Trial)
+	public void tearDown() {
+		myProducer.closeConnection();
+	}
+
+	@TearDown(Level.Iteration)
+	public void tearDownIteration() {
+	}
 
 }
