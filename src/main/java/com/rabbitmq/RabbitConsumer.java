b73ef612a64aaafce628af8504cefffe861dcbf9
Spencer Spitz <spencerspitz@gmail.com>
2021-06-21 16:53:00 -0400

    rabbitmq template

---
+++ src/main/java/com/rabbitmq/RabbitConsumer.java
@@ -0,0 +1,10 @@
+package com.rabbitmq;
+
+public class RabbitConsumer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/rabbitmq/RabbitProducerBenchmarks.java
@@ -0,0 +1,34 @@
+
+package com.rabbitmq;
+
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+
+@State(Scope.Benchmark)
+public class RabbitProducerBenchmarks {
+
+
+    @Setup(Level.Trial)
+    public void setup() {
+        //TODO Trial level: write code to be executed before each run of the benchmark
+    }
+
+    @Setup(Level.Iteration)
+    public void setupIteration() {
+        //TODO Iteration level: write code to be executed before each iteration of the benchmark.
+    }
+
+    @TearDown(Level.Trial)
+    public void tearDown() {
+        //TODO Trial level: write code to be executed after each run of the benchmark
+    }
+
+    @TearDown(Level.Iteration)
+    public void tearDownIteration() {
+        //TODO Iteration level: write code to be executed after each iteration of the benchmark.
+    }
+
+}
