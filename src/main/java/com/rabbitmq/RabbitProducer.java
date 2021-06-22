a8ae41369b8bea28f698754199dae857670ba57d
Spencer Spitz <spencerspitz@gmail.com>
2021-06-22 10:26:29 -0400

    base produce

--- src/main/java/com/rabbitmq/RabbitProducer.java
+++ src/main/java/com/rabbitmq/RabbitProducer.java
@@ -1,10 +1,35 @@
 package com.rabbitmq;
 
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
 public class RabbitProducer {
+	private static final String QUEUE_NAME = "TestQueue";
+
+	private void makeConnection() {
+		ConnectionFactory myFactory = new ConnectionFactory();
+		myFactory.setHost("localhost");
+
+		// try-with-resources to auto close the connection and channel after try block
+		// is completed
+		try (Connection myConnection = myFactory.newConnection(); Channel myChannel = myConnection.createChannel();) {
+			myChannel.queueDeclare(QUEUE_NAME, true, false, false, null);
+			String message = "Hello world";
+			myChannel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+			System.out.println("sent msg");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public RabbitProducer() {
+		makeConnection();
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		RabbitProducer p = new RabbitProducer();
 	}
 
 }
