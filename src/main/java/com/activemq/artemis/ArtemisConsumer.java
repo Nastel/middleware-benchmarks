c1a0287bbd7629485dab2a66d741593befe7a7e6
Spencer Spitz <spencerspitz@gmail.com>
2021-06-25 17:01:10 -0400

    reverted artemis classes to jms

--- src/main/java/com/activemq/artemis/ArtemisConsumer.java
+++ src/main/java/com/activemq/artemis/ArtemisConsumer.java
@@ -1,32 +1,36 @@
 package com.activemq.artemis;
 
-import org.apache.activemq.artemis.api.core.ActiveMQException;
-import org.apache.activemq.artemis.api.core.QueueConfiguration;
+import javax.jms.BytesMessage;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.Queue;
+
 import org.apache.activemq.artemis.api.core.TransportConfiguration;
-import org.apache.activemq.artemis.api.core.client.ActiveMQClient;
-import org.apache.activemq.artemis.api.core.client.ClientMessage;
-import org.apache.activemq.artemis.api.core.client.ClientConsumer;
-import org.apache.activemq.artemis.api.core.client.ClientSession;
-import org.apache.activemq.artemis.api.core.client.ClientSessionFactory;
-import org.apache.activemq.artemis.api.core.client.ServerLocator;
+import org.apache.activemq.artemis.api.jms.ActiveMQJMSClient;
+import org.apache.activemq.artemis.api.jms.JMSFactoryType;
 import org.apache.activemq.artemis.core.remoting.impl.netty.NettyConnectorFactory;
 
 public class ArtemisConsumer {
-	private final String QUEUE_NAME = "TestQueue";
-	private ClientSession mySession;
-	private ClientConsumer myConsumer;
+	private final String QUEUE_NAME = "MyQueue";
+	private Connection mConnection;
+	private Session mSession;
+	private MessageConsumer mConsumer;
 
 	public void buildConnection() {
-		ServerLocator locator = ActiveMQClient
-				.createServerLocatorWithoutHA(new TransportConfiguration(NettyConnectorFactory.class.getName()));
 		try {
-
-			ClientSessionFactory factory = locator.createSessionFactory();
-			mySession = factory.createSession();
-			myConsumer = mySession.createConsumer(QUEUE_NAME);
-			mySession.start();
-
-		} catch (Exception e) {
+			TransportConfiguration transportConfiguration = new TransportConfiguration(
+					NettyConnectorFactory.class.getName());
+			ConnectionFactory cf = ActiveMQJMSClient.createConnectionFactoryWithoutHA(JMSFactoryType.CF,
+					transportConfiguration);
+			mConnection = cf.createConnection();
+			mSession = mConnection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			Queue mQueue = mSession.createQueue(QUEUE_NAME);
+			mConsumer = mSession.createConsumer(mQueue);
+			mConnection.start();
+		} catch (JMSException e) {
 			e.printStackTrace();
 		}
 	}
@@ -38,8 +42,8 @@ public class ArtemisConsumer {
 	public void consume(int messagesToRead) {
 		for (int counter = 0; counter < messagesToRead; counter++) {
 			try {
-				ClientMessage message =  myConsumer.receive();
-			} catch (Exception e) {
+				BytesMessage message = (BytesMessage) mConsumer.receive();
+			} catch (JMSException e) {
 				e.printStackTrace();
 			}
 		}
@@ -47,8 +51,8 @@ public class ArtemisConsumer {
 
 	public void closeConnection() {
 		try {
-			mySession.close();
-		} catch (ActiveMQException e) {
+			mConnection.close();
+		} catch (JMSException e) {
 			e.printStackTrace();
 		}
 	}
