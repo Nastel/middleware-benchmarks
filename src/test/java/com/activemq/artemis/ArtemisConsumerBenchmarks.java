82adb9b3b8aaeb148efc8dac12815a9fe2652835
Spencer Spitz <spencerspitz@gmail.com>
2021-07-06 15:26:08 -0400

    removed concurrentconsume contexts

--- src/test/java/com/activemq/artemis/ArtemisConsumerBenchmarks.java
+++ src/test/java/com/activemq/artemis/ArtemisConsumerBenchmarks.java
@@ -22,8 +22,7 @@ import com.gocypher.cybench.core.annotation.BenchmarkMetaData;
 
 @State(Scope.Benchmark)
 @BenchmarkMetaData(key = "isLibraryBenchmark", value = "true")
-//@BenchmarkMetaData(key = "context", value = "ConcurrentConsume")
-//@BenchmarkMetaData(key = "context", value = "NonconcurrentConsume")
+@BenchmarkMetaData(key = "context", value = "NonconcurrentConsume")
 @BenchmarkMetaData(key = "domain", value = "java")
 public class ArtemisConsumerBenchmarks {
 	private ArtemisConsumer myConsumer;
