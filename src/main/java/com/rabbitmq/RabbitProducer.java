d8f2d4e38ce1fc26e64875217a02265dcfb3f937
danielrigos <85178707+danielrigos@users.noreply.github.com>
2021-07-07 16:54:46 -0400

    Update RabbitProducer.java
    updated to handle persistence boolean

--- src/main/java/com/rabbitmq/RabbitProducer.java
+++ src/main/java/com/rabbitmq/RabbitProducer.java
@@ -3,16 +3,25 @@ package com.rabbitmq;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.MessageProperties;
 import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.AMQP.BasicProperties;
 import com.rabbitmq.client.Channel;
 
 public class RabbitProducer {
 	private final String QUEUE_NAME = "MyQueue";
 	private Connection myConnection;
 	private Channel myChannel;
+	private boolean persistence = false;
+	private BasicProperties prop;
 
 	private void makeConnection() {
 		ConnectionFactory myFactory = new ConnectionFactory();
 		myFactory.setHost("localhost");
+
+		if (persistence) {
+			prop = MessageProperties.PERSISTENT_TEXT_PLAIN;
+		} else {
+			prop = MessageProperties.TEXT_PLAIN;
+		}
 
 		try {
 			myConnection = myFactory.newConnection();
@@ -33,7 +42,8 @@ public class RabbitProducer {
 
 			for (int counter = 0; counter < totalMessages; counter++) {
 				// use default exchange
-				myChannel.basicPublish("", QUEUE_NAME, MessageProperties.TEXT_PLAIN, byteArrMsg); //change to MessageProperties.PERSISTENT_TEXT_PLAIN to enable persistence 
+				myChannel.basicPublish("", QUEUE_NAME, prop, byteArrMsg);
+
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
