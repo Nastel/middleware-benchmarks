8574051520d6b0aefcb3def3491be60b087177b1
Spencer Spitz <53101920+spencerspitz@users.noreply.github.com>
2021-07-07 13:14:29 -0400

    persist condition and main method for consumption prefill

--- src/main/java/com/activemq/classic/ActiveMQProducer.java
+++ src/main/java/com/activemq/classic/ActiveMQProducer.java
@@ -11,7 +11,10 @@ import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 public class ActiveMQProducer {
-	private final String QUEUE_NAME = "MyQueue";
+	// PARAMS
+	private boolean persistence = false;
+	
+	private static String QUEUE_NAME = "MyQueue";
 	private Connection connection;
 	private Session session;
 	private MessageProducer producer;
@@ -25,7 +28,14 @@ public class ActiveMQProducer {
 			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 			destination = session.createQueue(QUEUE_NAME);
 			producer = session.createProducer(destination);
-			producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+			
+			if (persistence){
+				producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+			}
+			else{
+				producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+			}
+			
 			connection.start();
 
 		} catch (JMSException e) {
@@ -60,6 +70,31 @@ public class ActiveMQProducer {
 	}
 
 	public static void main(String[] args) {
+		// populates topic for the consumer benchmarks
+		QUEUE_NAME = "MyQueue1";
+		ActiveMQProducer p1 = new ActiveMQProducer();
+		p1.produce(130000, 512);
+		p1.closeConnection();
+
+		QUEUE_NAME = "MyQueue2";
+		ActiveMQProducer p2 = new ActiveMQProducer();
+		p2.produce(130000, 1024);
+		p2.closeConnection();
+
+		QUEUE_NAME = "MyQueue3";
+		ActiveMQProducer p3 = new ActiveMQProducer();
+		p3.produce(130000, 10240);
+		p3.closeConnection();
+
+		QUEUE_NAME = "MyQueue4";
+		ActiveMQProducer p4 = new ActiveMQProducer();
+		p4.produce(130000, 32768);
+		p4.closeConnection();
+
+		QUEUE_NAME = "MyQueue5";
+		ActiveMQProducer p5 = new ActiveMQProducer();
+		p5.produce(130000, 65536);
+		p5.closeConnection();
 	}
 
 }
